4a5ccd757af2cd20d9a61be2bf475a1cfad99c0a
Joost Yervante Damad <joost@damad.be>
2025-01-06 18:32:49 +0100

    cleanups. day 1 part 2

--- day01/day01.hs
+++ day01/day01.hs
@@ -11,8 +11,8 @@ readLines2 fileName = do
     contents <- readFile fileName
     return $ lines contents
 
-listToNumbers :: [String] -> [Integer]
-listToNumbers = map read
+listToNums :: (Num a, Read a) => [String] -> [a]
+listToNums = map read
 
 part :: (Eq a, Show a) => Int -> a -> FilePath -> ([String] -> a) -> IO ()
 part number expected filename op = do
@@ -30,24 +30,33 @@ part1 = part 1
 part2 :: (Eq a, Show a) => a -> FilePath -> ([String] -> a) -> IO ()
 part2 = part 2
 
-example = do
-    contents <- readLines "example.txt"
-    let numbers = map (listToNumbers . words) contents
-        leftNumbers = sort (map head numbers)
-        rightNumbers = sort (map last numbers)
-        result = sum (zipWith (\x y -> abs (x - y)) leftNumbers rightNumbers)
-    putStr (show result)
+part1_example = do
+    part1 11 "example.txt" day01part1
 
-example2 = do
-    part1 11 "example.txt" day01part01
+part1_example' :: IO ()
+part1_example' = do
+    part1 11 "example.txt" day01part1'
 
-example2' = do
-    part1 11 "example.txt" day01part01'
+part1_input = do
+    part1 2057374 "input.txt" day01part1'
 
-day01part01 :: [String] -> Integer
-day01part01 xs = sum $ zipWith (\x y -> abs (x - y)) (sort $ map (head . listToNumbers . words) xs) (sort $ map (last . listToNumbers . words) xs)
+part2_example = do
+    part2 31 "example.txt" day01part2
+
+part2_input = do
+    part2 23177084 "input.txt" day01part2
+
+day01part1 :: [String] -> Integer
+day01part1 xs = sum $ zipWith (\x y -> abs (x - y)) (sort $ map (head . listToNums . words) xs) (sort $ map (last . listToNums . words) xs)
 
 
-day01part01' :: [String] -> Integer
-day01part01' xs = sum $ zipWith (\x y -> abs (x - y)) (convert head) (convert last)
-    where convert taker = sort $ map (taker . listToNumbers . words) xs
+day01part1' :: [String] -> Integer
+day01part1' xs = sum $ zipWith (\x y -> abs (x - y)) (convert head) (convert last)
+    where convert taker = sort $ map (taker . listToNums . words) xs
+
+
+day01part2 :: [String] -> Int
+day01part2 xs = sum $ map (\x -> x * length (filter (== x) lasts)) firsts
+    where convert taker = sort $ map (taker . listToNums . words) xs
+          firsts = convert head
+          lasts = convert last
