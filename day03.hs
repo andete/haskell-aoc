9a01b02763533dcbd44d46191ef580e2f76505bd
Joost Yervante Damad <joost@damad.be>
2025-01-07 09:45:21 +0100

    day 3 part 2

--- day03.hs
+++ day03.hs
@@ -8,11 +8,20 @@ part1_input = do
     part1 160672468 "day03/input.txt" day03part1
 
 
-data Token = Mul | Number Integer | Comma | Close | Other
+
+part2_example = do
+    part1 48 "day03/example2.txt" day03part2
+
+part2_input = do
+    part1 84893551 "day03/input.txt" day03part2
+
+data Token = Mul | Number Integer | Comma | Close | Other | Do | Dont
     deriving (Show, Eq)
 
 tokenize :: String -> [Token]
 tokenize ('m':'u':'l':'(':xn) = Mul : tokenize xn
+tokenize ('d':'o':'(':')':xn) = Do : tokenize xn
+tokenize ('d':'o':'n':'\'':'t':'(':')':xn) = Dont : tokenize xn
 tokenize (x:xn) | isDigit x = tokenizeNumber x xn
 tokenize (',':xn) = Comma : tokenize xn
 tokenize (')':xn) = Close : tokenize xn
@@ -27,8 +36,15 @@ findValidMuls (Mul : Number x : Comma : Number y : Close : xn) = (x, y) : findVa
 findValidMuls (_:xs) = findValidMuls xs
 findValidMuls [] = []
 
-allValidMuls :: [String] -> [(Integer, Integer)]
-allValidMuls xs = findValidMuls (tokenize (concat xs))
-
 day03part1 :: [String] -> Integer
-day03part1 xs = sum $ map (uncurry (*)) (allValidMuls xs)
+day03part1 xs = sum $ map (uncurry (*)) (findValidMuls (tokenize (concat xs)))
+
+findValidMuls2 :: Bool -> [Token] -> [(Integer, Integer)]
+findValidMuls2 True (Mul : Number x : Comma : Number y : Close : xn) = (x, y) : findValidMuls2 True xn
+findValidMuls2 _ (Do : xn) = findValidMuls2 True xn
+findValidMuls2 _ (Dont : xn) = findValidMuls2 False xn
+findValidMuls2 a (_:xs) = findValidMuls2 a xs
+findValidMuls2 _ [] = []
+
+day03part2 :: [String] -> Integer
+day03part2 xs = sum $ map (uncurry (*)) (findValidMuls2 True (tokenize (concat xs)))
