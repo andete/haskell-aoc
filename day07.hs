24e0a60d8050026f10802bb21858827016166826
Joost Yervante Damad <joost@damad.be>
2025-01-13 14:20:17 +0100

    day 7 part 2

--- day07.hs
+++ day07.hs
@@ -1,8 +1,19 @@
 import Aoc
 import Debug.Trace (trace)
+import Text.Printf (printf)
 
 part1_example = do
     part1 3749 "day07/example.txt" day07part1
+
+part1_input = do
+    part1 850435817339 "day07/input.txt" day07part1
+    
+
+part2_example = do
+    part2 11387 "day07/example.txt" day07part2
+
+part2_input = do
+    part2 104824810233437 "day07/input.txt" day07part2
 
 
 parse:: String -> (Integer, [Integer])
@@ -14,11 +25,17 @@ parse s =
           first = head w
 
 canMake :: Integer -> [Integer] -> Bool
-canMake _ [] = False
 canMake a [b] = a == b 
-canMake a (b:_) | b >= a = False
-canMake a (b:c:bs) = canMake a ((b * c) : bs) || canMake a ((b + c) : bs)
+canMake a (b:c:bs) = canMake a ((b + c) : bs) || canMake a ((b * c) : bs)
+
+canMake2 :: Integer -> [Integer] -> Bool
+canMake2 a [b] = a == b 
+canMake2 a (b:c:bs) = canMake2 a ((b * c) : bs) || canMake2 a ((b + c) : bs) || canMake2 a (read (printf "%d%d" b c) : bs)
 
 day07part1 :: [String] -> Integer
 day07part1 field = sum $ map fst $ filter (uncurry canMake) parsed
+    where parsed = map parse field
+
+day07part2 :: [String] -> Integer
+day07part2 field = sum $ map fst $ filter (uncurry canMake2) parsed
     where parsed = map parse field
