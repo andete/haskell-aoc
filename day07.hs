3d289ab7086b001f5de3915c82f6547cc983b350
Joost Yervante Damad <joost@damad.be>
2025-01-13 14:28:07 +0100

    document slower alternatives

--- day07.hs
+++ day07.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 import Aoc
 import Debug.Trace (trace)
 import Text.Printf (printf)
@@ -7,7 +8,7 @@ part1_example = do
 
 part1_input = do
     part1 850435817339 "day07/input.txt" day07part1
-    
+
 
 part2_example = do
     part2 11387 "day07/example.txt" day07part2
@@ -25,12 +26,15 @@ parse s =
           first = head w
 
 canMake :: Integer -> [Integer] -> Bool
-canMake a [b] = a == b 
+canMake a [b] = a == b
 canMake a (b:c:bs) = canMake a ((b + c) : bs) || canMake a ((b * c) : bs)
+--sadly is slower: canMake a (b:c:bs) = any (\f -> canMake a (f b c : bs)) [(+), (*)]
+
 
 canMake2 :: Integer -> [Integer] -> Bool
-canMake2 a [b] = a == b 
+canMake2 a [b] = a == b
 canMake2 a (b:c:bs) = canMake2 a ((b * c) : bs) || canMake2 a ((b + c) : bs) || canMake2 a (read (printf "%d%d" b c) : bs)
+-- sasly is slightly slower: canMake2 a (b:c:bs) = any (\f -> canMake2 a (f b c : bs)) [(+), (*), \x y -> read (printf "%d%d" x y)]
 
 day07part1 :: [String] -> Integer
 day07part1 field = sum $ map fst $ filter (uncurry canMake) parsed
