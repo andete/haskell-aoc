9404253e04ef23bd68f934449ef06257363d1bd1
Joost Yervante Damad <joost@damad.be>
2025-01-18 18:09:18 +0100

    day14 part 1

--- 2024/day14/day14.hs
+++ 2024/day14/day14.hs
@@ -19,15 +19,30 @@ parse s = Robot location velocity
           velocity = Location.fromList $ map read $ splitOn "," $ drop 2 $ w !! 1
 
 showRobotMaze :: Int -> Int -> [Robot] -> String
-showRobotMaze w h robots = unlines $ map (\y -> map (\x -> 
+showRobotMaze wide tall robots = unlines $ map (\y -> map (\x -> 
         let location = Location.fromList [x,y] in
         let count = length $ filter (== location) locations in
-        if count > 0 then chr (ord '0' + count) else '.') [0..(w-1)]) [0..(h-1)]
+        if count > 0 then chr (ord '0' + count) else '.') [0..(wide-1)]) [0..(tall-1)]
     where locations = map (\(Robot l _) -> l) robots
 
-part1_example = do
-    part1 480 "2024/day14/example.txt" day14part1
+move :: Int -> Int -> Robot -> Robot
+move wide tall (Robot (Location.Location x y) (Location.Location xv yv)) = 
+    Robot (Location.Location ((x + xv) `mod` wide) ((y + yv) `mod` tall)) (Location.Location xv yv)
 
-day14part1 :: [String] -> Integer
-day14part1 s = trace (showRobotMaze 11 7 robots) 0
+robotsInQuadrant :: [Robot] -> Int -> Int -> Int -> Int -> Integer
+robotsInQuadrant robots xMin xMax yMin yMax = toInteger $ length $ filter (\(Robot (Location.Location x y) _) -> x >= xMin && x <= xMax && y >= yMin && y <= yMax) robots
+
+part1_example = do
+    part1 12 "2024/day14/example.txt" $ day14part1 11 7
+
+part1_input = do
+    part1 218295000 "2024/day14/input.txt" $ day14part1 101 103
+
+day14part1 :: Int -> Int -> [String] -> Integer
+day14part1 wide tall s = q1 * q2 * q3 * q4
     where robots = map parse s
+          robots100 = foldl (\r _ -> map (move wide tall) r) robots [0..99]
+          q1 = robotsInQuadrant robots100 0 (wide `div` 2 - 1) 0 (tall `div` 2 - 1)
+          q2 = robotsInQuadrant robots100 (wide `div` 2 + 1) (wide - 1) 0 (tall `div` 2 - 1)
+          q3 = robotsInQuadrant robots100 0 (wide `div` 2 - 1) (tall `div` 2 + 1) (tall - 1)
+          q4 = robotsInQuadrant robots100 (wide `div` 2 + 1) (wide - 1) (tall `div` 2 + 1) (tall - 1)
