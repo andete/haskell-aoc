67aa4587f70ef4d5c451e71930c1560ae673b953
Joost Yervante Damad <joost@damad.be>
2025-01-26 18:27:37 +0100

    move "saving" up (doesn't speed it much though)

--- 2024/day20/day20.hs
+++ 2024/day20/day20.hs
@@ -32,8 +32,8 @@ part1pre_input = do
 
 -- part 1: for each position in the path, try doing cheats and see how much we can reduce the path length
 
-cheat :: Maze Char -> [Located Char] -> H.HashMap Int Int -> Located Char -> H.HashMap Int Int
-cheat maze path cr loc = cr'
+cheat :: Int -> Maze Char -> [Located Char] -> H.HashMap Int Int -> Located Char -> H.HashMap Int Int
+cheat saving maze path cr loc = cr'
     where neighFun loc = filter (\v -> value v /= '#') $ M.neighbours maze (location loc)
           -- look at neighbours and one step further
           neigh = M.neighboursDir maze (location loc)
@@ -42,14 +42,12 @@ cheat maze path cr loc = cr'
           neigh3 = filter (\(l1, l2) -> value l1 == '#' && value l2 /= '#') neigh2
           -- make sure if we cheat we end up on a position further in the path
           neigh4 = filter (\(l1, l2) -> fromJust (elemIndex loc path) < fromJust (elemIndex l2 path)) neigh3
-          -- insert the saved steps in the hashmap
+          -- insert the saved steps in the hashmap, but only if they save at least 'saving' steps
           cr' = foldl (\h (l1, l2) -> let d = fromJust (elemIndex l2 path) - fromJust (elemIndex loc path) - 2 in
-                H.insert d (H.lookupDefault 0 d h + 1) h) cr neigh4
-
-
+                if d >= saving then H.insert d (H.lookupDefault 0 d h + 1) h else h) cr neigh4
 
 day20part1 :: Int -> [String] -> Int
-day20part1 saving input = trace (show cr) $ trace (M.showMaze (: []) maze sp) $ sum $ H.elems cr'
+day20part1 saving input = trace (show cr) $ trace (M.showMaze (: []) maze sp) $ sum $ H.elems cr
     where maze = M.parse id input
           start = head $ M.findAll maze 'S'
           end = head $ M.findAll maze 'E'
@@ -57,11 +55,12 @@ day20part1 saving input = trace (show cr) $ trace (M.showMaze (: []) maze sp) $ 
           astar = AStar start (== end) (\_ _ -> 1) neigh (const 0)
           p = path astar
           sp = HS.fromList $ map location p
-          cr = foldl (cheat maze p) H.empty p
-          cr' = H.filterWithKey (\k _ -> k >= saving) cr
+          cr = foldl (cheat saving maze p) H.empty p
 
 part1_example = do
     part1 10 "2024/day20/example.txt" (day20part1 10)
 
 part1_input = do
     part1 1399 "2024/day20/input.txt" (day20part1 100)
+
+-- part 2: instead of cheating 1, we can do upto 20 cheats in one go
