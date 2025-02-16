09e822bd9a088e11e0fa3500bd27acaf5b028fe7
Joost Yervante Damad <joost@damad.be>
2025-02-16 08:15:29 +0100

    a

--- euler/0018.hs
+++ euler/0018.hs
@@ -8,6 +8,7 @@ maxPath a = head $ foldr1 f a
     where f row res = zipWith3 (\row1 res1 tRes1 -> row1 + max res1 tRes1) row res (tail res)
     -- replace the row by the sum of the row and the maximum of the two adjacent numbers in the result from the lower rows
     -- this means we're building the result from the bottom up
+    -- this is a result of using folr instead of foldl
 
 example = runLines 23 "euler/input/0018ex.txt" $ maxPath . parse
 
