87dd54e333bc0971fafa3f362897d416a248176a
Joost Yervante Damad <joost@damad.be>
2025-12-04 19:14:53 +0100

    fix names

--- aoc/2025/day02/day02.hs
+++ aoc/2025/day02/day02.hs
@@ -6,16 +6,16 @@ import Debug.Trace (trace)
 import Data.List.Split (splitOn)
 
 part1_example = do
-    part1 1227775554 "2025/day02/example.txt" day01part1
+    part1 1227775554 "2025/day02/example.txt" day02part1
 
 part1_input = do
-    part1 18893502033 "2025/day02/input.txt" day01part1
+    part1 18893502033 "2025/day02/input.txt" day02part1
 
 part2_example = do
-    part2 4174379265 "2025/day02/example.txt" day01part2
+    part2 4174379265 "2025/day02/example.txt" day02part2
 
 part2_input = do
-    part2 26202168557 "2025/day02/input.txt" day01part2
+    part2 26202168557 "2025/day02/input.txt" day02part2
 
 
 range :: String -> [Integer]
@@ -40,13 +40,13 @@ invalid2 x = any (okay s) options
           halve = length s `div` 2
           options = [1..halve]
 
-day01part1 :: [String] -> Integer
-day01part1 xn = sum invalids
+day02part1 :: [String] -> Integer
+day02part1 xn = sum invalids
    where ranges = map range $ splitOn "," (head xn)
          invalids = concat $ map (\r -> filter invalid r) ranges
 
 
-day01part2 :: [String] -> Integer
-day01part2 xn = sum invalids
+day02part2 :: [String] -> Integer
+day02part2 xn = sum invalids
    where ranges = map range $ splitOn "," (head xn)
          invalids = concat $ map (\r -> filter invalid2 r) ranges
